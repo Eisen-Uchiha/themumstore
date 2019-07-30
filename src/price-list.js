903ef6bf4347b2654adf6cb6938c24e6aae1e52a
Eisen Uchiha <eisenuchiha@gmail.com>
2019-07-29 23:44:45 -0500

    Added Price List

---
+++ src/price-list.js
@@ -0,0 +1,70 @@
+const prices = {
+  main: {
+    spiritBadge: {
+      mums: 10,
+      garters: 10,
+      loops: 0.01,
+      boa: 8.00,
+      bling: null,
+      extraWidth: null,
+      twoTone: 7.00,
+    },
+
+    mini: {
+      mums: 15,
+      garters: 15,
+      loops: 0.01,
+      boa: 8.00,
+      bling: null,
+      extraWidth: null,
+      twoTone: 7.00,
+    },
+
+    small: {
+      mums: 32,
+      garters: 27,
+      loops: 0.01,
+      boa: 8.00,
+      bling: 8.00,
+      extraWidth: 5.00,
+      twoTone: 7.00,
+    },
+
+    medium: {
+      mums: 50,
+      garters: 40,
+      loops: 0.01,
+      boa: 8.00,
+      bling: 10.00,
+      extraWidth: 7.00,
+      twoTone: 7.00,
+    },
+
+    large: {
+      mums: 70,
+      garters: 55,
+      loops: 0.01,
+      boa: 8.00,
+      bling: 12.00,
+      extraWidth: 9.00,
+      twoTone: 7.00,
+    },
+
+    xl: {
+      mums: 85,
+      garters: 65,
+      loops: 0.01,
+      boa: 8.00,
+      bling: 15.00,
+      extraWidth: null,
+      twoTone: 7.00,
+    },
+  },
+
+  extras: {
+    extra1: 5.00,
+    extra2: 5.00,
+  }
+}
+
+export default prices
