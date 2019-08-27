55dab8e63b48c1b162cab8d1688a3d4066c250c0
Eisen Uchiha <eisenuchiha@gmail.com>
2019-08-27 17:16:13 -0500

    Testing Lambda

--- src/Park.js
+++ src/Park.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 function Park() {
   return (
-    <div>Under Construction</div>
+    <div>
+      <div>Under Construction</div>
+      <div><button onClick={handleClick("hello")}>Test</button></div>
+    </div>
   )
+}
+
+const handleClick = api => e => {
+  e.preventDefault()
+
+  // this.setState({ loading: true })
+  fetch("/.netlify/functions/" + api)
+    .then(response => response.json())
+    .then(json => console.log(json))
 }
 
 export default Park
