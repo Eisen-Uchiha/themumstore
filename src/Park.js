11e6bde8df03af397ff6d27f7fc9afb4a8727b8d
Eisen Uchiha <eisenuchiha@gmail.com>
2019-08-27 17:44:38 -0500

    Testing

--- src/Park.js
+++ src/Park.js
@@ -14,7 +14,7 @@ const handleClick = api => e => {
 
   // this.setState({ loading: true })
   fetch("/.netlify/functions/" + api)
-    .then(response => response.json())
+    .then(response => { console.log(response); response.json(); })
     .then(json => console.log(json))
 }
 
