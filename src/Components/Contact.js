6b710daec76988f0d08c92006ade765e39f15095
Eisen Uchiha <eisenuchiha@gmail.com>
2019-09-01 05:55:28 -0500

    Flex on Contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Input, Button, Checkbox, Icon, Tooltip } from 'antd'
+import { Input, Button, Checkbox, Icon, Tooltip, Content, Layout } from 'antd'
 
 class Contact extends Component {
   state = {
@@ -45,61 +45,70 @@ class Contact extends Component {
     const { name, email, message, sendCart } = this.state.fields
   
     return (
-      <section style={{ padding: '1% 2%' }}>
+      <div style={{ padding: '1% 2%', background: 'white' }}>
         <h1 style={{ textAlign: 'center' }}>This is the Contact Page</h1>
-        <div className='contact-wrap'>
-          <div className='contact-left'>
+        <div className='contact-wrap cards'>
+          <div className='contact-left card' style={{ flex: '1 1 26%' }}>
             <form ref={node => this.form = node}>
-              <Input
-                className='contact-field'
-                ref={node => this.name = node}
-                id='name'
-                size='large'
-                placeholder='Name'
-                value={name.value}
-                onChange={this.handleChange}
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                suffix={
-                  <Tooltip title={name.value.length < 2 ? 'Enter your name' : 'Required Field'}>
-                    <Icon type="info-circle" style={{ color: name.value.length < 2 ? 'red' : '#68C6BF' }} />
-                  </Tooltip>
-                }
-              />
-              <Input
-                className='contact-field'
-                ref={node => this.email = node}
-                id='email'
-                size='large'
-                placeholder='Email'
-                value={email.value}
-                onChange={this.handleChange}
-                prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                suffix={
-                  <Tooltip title={email.value.length < 10 ? 'Enter a valid email' : 'Required Field'}>
-                    <Icon type="info-circle" style={{ color: email.value.length < 10 ? 'red' : '#68C6BF' }} />
-                  </Tooltip>
-                }
-              />
-              <Input.TextArea
-                className='contact-field'
-                ref={node => this.message = node}
-                id='message'
-                placeholder='Message Required'
-                value={message.value}
-                onChange={this.handleChange}
-              />
+              <div>
+                <Input
+                  className='contact-field'
+                  ref={node => this.name = node}
+                  id='name'
+                  size='large'
+                  placeholder='Name'
+                  value={name.value}
+                  onChange={this.handleChange}
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  suffix={
+                    <Tooltip title={name.value.length < 2 ? 'Enter your name' : 'Required Field'}>
+                      <Icon type="info-circle" style={{ color: name.value.length < 2 ? 'red' : '#68C6BF' }} />
+                    </Tooltip>
+                  }
+                />
+              </div>
+              <div>
+                <Input
+                  className='contact-field'
+                  ref={node => this.email = node}
+                  id='email'
+                  size='large'
+                  placeholder='Email'
+                  value={email.value}
+                  onChange={this.handleChange}
+                  prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  suffix={
+                    <Tooltip title={email.value.length < 10 ? 'Enter a valid email' : 'Required Field'}>
+                      <Icon type="info-circle" style={{ color: email.value.length < 10 ? 'red' : '#68C6BF' }} />
+                    </Tooltip>
+                  }
+                />
+              </div>
+              <div>
+                <Input.TextArea
+                  className='contact-field'
+                  ref={node => this.message = node}
+                  id='message'
+                  placeholder='Message Required'
+                  value={message.value}
+                  autosize={{ minRows: 2, maxRows: 4 }}
+                  onChange={this.handleChange}
+                />
+              </div>
               <div className='contact-field'>Send Cart Information <Checkbox ref={node => this.sendCart = node} id='sendCart' checked={sendCart.value} onChange={this.handleChange} /></div>
+              <div>
               <Button className={!name.formatted || !email.formatted || !message.formatted ? 'submit-disabled' : ''} onClick={this.handleSubmit}>Submit</Button>
+              </div>
             </form>
           </div>
-          <div className='contact-right'>
+          <div className='contact-right card'>
             <h2>Connect with us:</h2>
             <div><Icon type="facebook" style={{ fontSize: 'xx-large', backgroundColor: 'white', color: '#3b5998' }}/></div>
             <div style={{ marginTop: '10px' }}>For support or any questions:</div>
             <div>Email us at {<a href="mailto:support@pixpa.com" target="_blank" rel="noopener noreferrer">support@boutiquemums.com</a>}</div>
           </div>
         </div>
-      </section>
+      </div>
     )
   }
 }
