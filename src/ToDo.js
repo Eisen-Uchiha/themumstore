98ca366bde596a5105eacfd48c1ed84271a7d388
Eisen Uchiha <eisenuchiha@gmail.com>
2019-09-06 05:52:13 -0500

    Updated To Do List

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,26 +1,23 @@
-//(1) Add pictures to website
-
-//(1) Add a way for people to see enlarged pictures on carousel and gallery
-
-//(2) Implement a way to cut off orders by a certain date
-
-//(3) Show required fields in customization
-
-//(4) Combine Mums and Gaters pages
-
-//(5) Allow deselect in accent color dropdown
-
-//(6) Add pop out descriptions of extras, preferrably with pictures
-
-//(7) Style the info in Product Card under the image to save text space in explaining the dimensions and extras
-
-//(8) Add 3D trinkets as an extra
-  // Separate section from other extras
-  // Selecting option makes another line for another 3D trinket
-
-//(9) Reconfig "custom die cuts"
+//(1) Reconfig "custom die cuts"
   // First Name, School Name, Custom Word, School Mascot Name, School Mascot Logo, Cheer Package, Senior Package
   // Separate section from other extras
   // Selecting option makes another line for another die cut configuration
   // Option line should be button group to select between dropdown that includes the specific mascot images in inventory (Picture Optional)
+
+//(2) Add pictures to website
+
+//(3) Show required fields in customization
+
+//(4) Allow deselect in accent color dropdown
+
+//(5) Style the info in Product Card under the image to save text space in explaining the dimensions and extras
+
+//(6) Implement a way to cut off orders by a certain date
+
+//(7) Add a way for people to see enlarged pictures on carousel and gallery
+
+//(8) Add pop out descriptions of extras, preferrably with pictures
+
+//(9) Combine Mums and Gaters pages
+
 
